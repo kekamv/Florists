12e1399f5a9dce7d7e0f7a8994c7feb5e9677bd5
keka <raquel2001mv@hotmail.com>
2020-07-15 15:42:00 +0200

    class Product added

--- src/Products/Product.java
+++ src/Products/Product.java
@@ -1,4 +1,35 @@
 package Products;
 
-public class Product {
+import java.time.LocalDate;
+
+public abstract class Product {
+
+    private int id;
+    private String type;
+    private double price;
+    private LocalDate inStockSince;
+    private int idCounter =1;
+
+    protected Product(double price){
+        id= idCounter;
+        this.price = price;
+        inStockSince = LocalDate.now();
+
+        idCounter++;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public LocalDate getInStockSince(){
+        return inStockSince;
+    }
+
+    @Override
+    public abstract String toString();
 }
