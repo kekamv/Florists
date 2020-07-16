3e4ae8207f964b7b69346535d2dafab07eea8fcf
EnricCL <enric.comes@gmail.com>
2020-07-16 11:01:35 +0200

    MainFlorist added

---
+++ src/MainFlorist.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**Main of the program
+ * @author enric
+ */
+
+public class MainFlorist {
+
+    public static void main(String[] args){
+
+        String name;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the name of the florist: ");
+        name = input.nextLine();
+
+        //The menu of interaction with the Florist is created
+        MenuFlorist menuFlorist = new MenuFlorist (name);
+
+        //Runs the interacting menu
+        menuFlorist.menuFlorist();
+
+        input.close();
+    }
+}
